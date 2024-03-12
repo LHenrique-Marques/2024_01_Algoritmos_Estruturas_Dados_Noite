aea2fc361db2233a5bc4fa37a7a7efcfec166c65
Henrique Marques <henrique.marquesvj@gmail.com>
2024-03-11 21:34:58 -0300

    teste

---
+++ Aula02/main.py
@@ -0,0 +1,15 @@
+class Produto():
+    def __init__(self,nome,preco,quantidade):
+        self.nome = nome
+        self.preco = preco
+        self.quantidade = quantidade
+    
+    def __str__(self):
+        print(f"""
+              
+              
+              """)
+        
+pepsi = Produto("Pepsi",10.50,30)
+
+print(pepsi)
