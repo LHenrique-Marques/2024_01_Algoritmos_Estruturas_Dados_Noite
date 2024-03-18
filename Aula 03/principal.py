9c45994a7744b525719e097ad3f8ea58be21d4c4
Henrique Marques <henrique.marquesvj@gmail.com>
2024-03-18 19:27:10 -0300

    adicionando aula 3

---
+++ Aula 03/principal.py
@@ -0,0 +1,14 @@
+class Cidade():
+    def __init__(self,id,nome):
+        self.id = int(id)
+        self.nome = str(nome)
+
+
+
+class Pessoa():
+    def __init__(self):
+        self.id = int()
+        self.nome = str()
+        self.idade = int()
+        self.cidade = Cidade(12345,"Porto Alegre")
+        
